29c699d92ff173b03dadc82c8fcb5edc82d76300
Karol Czyżewski <Karolek0493@wp.pl>
2021-01-05 22:03:18 +0100

    add: hover -> items

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,8 @@ function switchMode() {
     document.documentElement.style.setProperty('--dark-grayish-blue', 'hsl(228, 34%, 66%)');
     document.documentElement.style.setProperty('--very-dark', 'hsl(0, 0%, 100%)');
     document.documentElement.style.setProperty('--toggle-light', 'linear-gradient(76deg, rgba(55,143,230,1) 24%, rgba(62,218,130,1) 68%)');
+    document.documentElement.style.setProperty('--hover-color', 'hsl(228, 22%, 40%)');
+
 }
 
 button.addEventListener('click', () => {
